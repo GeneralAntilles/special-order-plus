21522cc4ea298600391297311ab518e58921ce1a
Ryan Abel <rabelg5@gmail.com>
2014-12-30 09:31:05 -0500

    Add printer option
    Add a dropdown for selecting which printer we want to print to. Hardcoded for now.

--- ipage-plus.js
+++ ipage-plus.js
@@ -144,6 +144,10 @@ $( "<div style='display: none'> \
 <label class='checkbox'><input type='checkbox' name='orderInfo[paid]' accesskey='p' value='Paid'>Paid<br></label> \
 <input type='hidden' value='Off' name='orderInfo[stock]'> \
 <label class='checkbox'><input type='checkbox' name='orderInfo[stock]' accesskey='i' value='Stock'>Stock<br></label> \
+<label><span>Printer: </span><select style='font-size: 1.2em;' name='orderInfo[printer]'> \
+  <option value='Brother_HL-5470DW'>Front desk - Brother HL-5470DW</option> \
+  <option value='Brother_HL-2270DW'>Warehouse - Brother HL-2270DW</option> \
+</select></label> \
 </div> \
 <div class='formRight' style='float: right; width: 50%;'> \
 <input type='hidden' value='NoShip' name='orderInfo[ship]'> \
