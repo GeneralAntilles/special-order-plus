25e2de940aee57b25ca69186d0753e8132d11c91
GeneralAntilles <rabelg5@gmail.com>
2014-12-30 21:19:52 -0500

    Fix inconsistent variable naming style

--- special-order.php
+++ special-order.php
@@ -45,15 +45,15 @@ if ( empty( $orderInfo["address"] ) ) {
 }
 
 // This is the PDF form template URL
-$pdf_file_url = "https://" . PDF_HOSTNAME . "/special-order-form-ingram.pdf";
-$pdf_template_path = "/special-order-form-ingram.pdf";
+$pdfFileUrl = "https://" . PDF_HOSTNAME . "/special-order-form-ingram.pdf";
+$pdfTemplatePath = "/special-order-form-ingram.pdf";
 
 // This is the pdftk tool path
 $pdftk = "/usr/bin/pdftk";
 
 // Generate a unique filename for each order
 $filename = date( "Y-m-d" ) . "-" . time();
-$xfdf_file = $filename . ".xfdf";
+$xfdfFile = $filename . ".xfdf";
 $result_directory = dirname(__FILE__) . "/" . RESULTS_PATH;
 
 // Get the counter index from a file and increment it by one
@@ -70,26 +70,26 @@ fclose($fp);
 $orderInfo["orderNumber"] = file_get_contents ( "./special-order-index.txt" );
 
 // This is where our XFDF file ends up
-$xfdf_file_path = $result_directory . "/" . $xfdf_file;
+$xfdfFilePath = $result_directory . "/" . $xfdfFile;
 
 // PDF file names
-$pdf_name = substr( $xfdf_file_path, 0, -4 ) . "pdf";
-$pdf_file_name = $filename . ".pdf";
+$pdfName = substr( $xfdfFilePath, 0, -4 ) . "pdf";
+$pdfFileName = $filename . ".pdf";
 
 // Generate the XFDF
-$xfdf = createXFDF( $pdf_file_url, $orderInfo );
+$xfdf = createXFDF( $pdfFileUrl, $orderInfo );
 
 // Write the XFDF to disk
-if( $fp = fopen( $xfdf_file_path, "w" ) )
+if( $fp = fopen( $xfdfFilePath, "w" ) )
 {
     fwrite( $fp, $xfdf, strlen( $xfdf ) );
 }
 fclose($fp);
 
 // Fill the PDF from the XFDF, generate an EAN13 barcode, stamp the PDF with the barcode
-$xfdfCmd = "$pdftk $pdf_template_path fill_form $xfdf_file_path output $pdf_name.pre flatten";
+$xfdfCmd = "$pdftk $pdfTemplatePath fill_form $xfdfFilePath output $pdfName.pre flatten";
 $barcodeCmd = "/usr/local/bin/barcode -e ean13 -n -b \"$orderInfo[ean13]\" -D -o " . RESULTS_PATH . "$filename-13.txt -g100x30+160+135";
-$stampCmd = "$pdftk $pdf_name.pre stamp " . RESULTS_PATH . "$filename-13.pdf output $pdf_name";
+$stampCmd = "$pdftk $pdfName.pre stamp " . RESULTS_PATH . "$filename-13.pdf output $pdfName";
 
 exec( $xfdfCmd, $outputXfdf, $retXfdf );
 exec( $barcodeCmd, $outputBarcode, $retBarcode );
@@ -110,7 +110,7 @@ if ( file_exists( "./" . RESULTS_PATH . $filename . ".pdf.pre" ) ) {
 }
 
 // Print the final PDF
-system( "/usr/bin/lpr -o landscape -P " . $orderInfo["printer"] . " " . RESULTS_PATH . "$pdf_file_name" ); 
+system( "/usr/bin/lpr -o landscape -P " . $orderInfo["printer"] . " " . RESULTS_PATH . "$pdfFileName" ); 
 
 echo json_encode($orderInfo);
 ?>
