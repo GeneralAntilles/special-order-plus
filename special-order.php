3338d36c1ef0b5c01f276ea207deb4de0597772a
GeneralAntilles <rabelg5@gmail.com>
2014-12-30 21:02:35 -0500

    Add setting for results folder
    The generated files will end up in the RESULTS_PATH folder.

--- special-order.php
+++ special-order.php
@@ -1,59 +1,65 @@
-<?php require ( $_SERVER['DOCUMENT_ROOT'] . '/create-xfdf.php' ); ?>
+<?php require ( $_SERVER["DOCUMENT_ROOT"] . "/create-xfdf.php" ); ?>
 
 <?php
-header('Access-Control-Allow-Origin: *'); 
+header("Access-Control-Allow-Origin: *"); 
 
 /* Settings */
 
-define( 'PDF_HOSTNAME', 'web.haslams' );
-define( 'PDFTK', '/usr/bin/pdftk' ); 
+// Hostname of the server with the PDF template
+define( "PDF_HOSTNAME", "web.haslams" );
+
+// Path to our pdftk executable
+define( "PDFTK", "/usr/bin/pdftk" );
+
+// Where we want to output our files. Needs a trailing slash. 
+define( "RESULTS_PATH", "results/");
 
 /* Order variables */
 
 // Fill our array from the form POST
-$orderInfo = $_POST['orderInfo'];
+$orderInfo = $_POST["orderInfo"];
 
 // Append the last name to the first name for display in the PDF
-$orderInfo['name'] = $orderInfo['firstName'] . ' ' . $orderInfo['lastName'];
+$orderInfo["name"] = $orderInfo["firstName"] . " " . $orderInfo["lastName"];
 
 // If we have city/state info:
-if ( !empty( $orderInfo['city'] ) ) {
+if ( !empty( $orderInfo["city"] ) ) {
 	// Append the state abbreviation to the city for display in the PDF
-	$orderInfo['cityState'] = $orderInfo['city'] . ', ' . $orderInfo['state'];
+	$orderInfo["cityState"] = $orderInfo["city"] . ", " . $orderInfo["state"];
 }
 
 // Set the date for the order
-$orderInfo['orderDate'] = date( 'F jS, Y' );
+$orderInfo["orderDate"] = date( "F jS, Y" );
 
 // This is our PDF form template path for the special order
-if ( $orderInfo['distributor'] == "ingram" ) {
-	$pdf_template_distributor = 'ingram';
+if ( $orderInfo["distributor"] == "ingram" ) {
+	$pdf_template_distributor = "ingram";
 } else {
-	$pdf_template_distributor = 'bt';
+	$pdf_template_distributor = "bt";
 }
 
-if ( empty( $orderInfo['address'] ) ) {
-	$pdf_template_type = 'name-phone-';
+if ( empty( $orderInfo["address"] ) ) {
+	$pdf_template_type = "name-phone-";
 } else {
-	$pdf_template_type = '';
+	$pdf_template_type = "";
 }
 
 // This is the PDF form template URL
-$pdf_file_url = 'https://' . PDF_HOSTNAME . '/special-order-form-ingram.pdf';
-$pdf_template_path = '/special-order-form-ingram.pdf';
+$pdf_file_url = "https://" . PDF_HOSTNAME . "/special-order-form-ingram.pdf";
+$pdf_template_path = "/special-order-form-ingram.pdf";
 
 // This is the pdftk tool path
-$pdftk = '/usr/bin/pdftk';
+$pdftk = "/usr/bin/pdftk";
 
 // Generate a unique filename for each order
-$filename = date( 'Y-m-d' ) . '-' . time();
+$filename = date( "Y-m-d" ) . "-" . time();
 $xfdf_file = $filename . ".xfdf";
-$result_directory = dirname(__FILE__) . '/results';
+$result_directory = dirname(__FILE__) . "/" . RESULTS_PATH;
 
 // Get the counter index from a file and increment it by one
-$fp = fopen('special-order-index.txt', 'c+');
+$fp = fopen("special-order-index.txt", "c+");
 flock($fp, LOCK_EX);
-$count = (int)fread($fp, filesize('special-order-index.txt'));
+$count = (int)fread($fp, filesize("special-order-index.txt"));
 ftruncate($fp, 0);
 fseek($fp, 0);
 fwrite($fp, $count +1 );
@@ -61,20 +67,20 @@ flock($fp, LOCK_UN);
 fclose($fp);
 
 // Set the orderNumber from the counter index for display in the PDF
-$orderInfo['orderNumber'] = file_get_contents ( './special-order-index.txt' );
+$orderInfo["orderNumber"] = file_get_contents ( "./special-order-index.txt" );
 
-// This is where our XFDF file ends up
-$xfdf_file_path = $result_directory . '/' . $xfdf_file;
+// This is where our XFDF file ends up
+$xfdf_file_path = $result_directory . "/" . $xfdf_file;
 
 // PDF file names
-$pdf_name = substr( $xfdf_file_path, 0, -4 ) . 'pdf';
-$pdf_file_name = $filename . '.pdf';
+$pdf_name = substr( $xfdf_file_path, 0, -4 ) . "pdf";
+$pdf_file_name = $filename . ".pdf";
 
 // Generate the XFDF
 $xfdf = createXFDF( $pdf_file_url, $orderInfo );
 
 // Write the XFDF to disk
-if( $fp = fopen( $xfdf_file_path, 'w' ) )
+if( $fp = fopen( $xfdf_file_path, "w" ) )
 {
     fwrite( $fp, $xfdf, strlen( $xfdf ) );
 }
@@ -82,29 +88,29 @@ fclose($fp);
 
 // Fill the PDF from the XFDF, generate an EAN13 barcode, stamp the PDF with the barcode
 $xfdfCmd = "$pdftk $pdf_template_path fill_form $xfdf_file_path output $pdf_name.pre flatten";
-$barcodeCmd = "/usr/local/bin/barcode -e ean13 -n -b \"$orderInfo[ean13]\" -D -o results/$filename-13.txt -g100x30+160+135";
-$stampCmd = "$pdftk $pdf_name.pre stamp results/$filename-13.pdf output $pdf_name";
+$barcodeCmd = "/usr/local/bin/barcode -e ean13 -n -b \"$orderInfo[ean13]\" -D -o " . RESULTS_PATH . "$filename-13.txt -g100x30+160+135";
+$stampCmd = "$pdftk $pdf_name.pre stamp " . RESULTS_PATH . "$filename-13.pdf output $pdf_name";
 
 exec( $xfdfCmd, $outputXfdf, $retXfdf );
 exec( $barcodeCmd, $outputBarcode, $retBarcode );
-exec( "cat barcode_doc_front.txt results/$filename-13.txt barcode_doc_back.txt > results/$filename-13.pdf", $outputCat, $retCat);
+exec( "cat barcode_doc_front.txt " . RESULTS_PATH ."$filename-13.txt barcode_doc_back.txt > " . RESULTS_PATH . "$filename-13.pdf", $outputCat, $retCat);
 exec( $stampCmd, $outputStamp, $retStamp );
 
-if ( file_exists( './results/' . $filename . '-13.pdf' ) ) {
+if ( file_exists( "./" . RESULTS_PATH . $filename . "-13.pdf" ) ) {
 
-	unlink( './results/' . $filename . '-13.pdf' );
+	unlink( "./" . RESULTS_PATH . $filename . "-13.pdf" );
 }
 
-if ( file_exists( './results/' . $filename . '-13.txt' ) ) {
-	unlink( './results/' . $filename . '-13.txt' );
+if ( file_exists( "./" . RESULTS_PATH . $filename . "-13.txt" ) ) {
+	unlink( "./" . RESULTS_PATH . $filename . "-13.txt" );
 }
 
-if ( file_exists( './results/' . $filename . '.pdf.pre' ) ) {
-	unlink( './results/' . $filename . '.pdf.pre' );
+if ( file_exists( "./" . RESULTS_PATH . $filename . ".pdf.pre" ) ) {
+	unlink( "./" . RESULTS_PATH . $filename . ".pdf.pre" );
 }
 
 // Print the final PDF
-system( "/usr/bin/lpr -o landscape -P Brother_HL-5470DW results/$pdf_file_name" ); 
+system( "/usr/bin/lpr -o landscape -P Brother_HL-5470DW " . RESULTS_PATH . "$pdf_file_name" ); 
 
 echo json_encode($orderInfo);
 ?>
